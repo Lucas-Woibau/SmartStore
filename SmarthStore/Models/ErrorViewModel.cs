cec86ecdcef393931d76605b2aca3a5c3e620d59
Lucas Woibau <lucaswoibau7@gmail.com>
2024-07-02 17:10:54 -0300

    Add project files.

---
+++ SmarthStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmarthStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SmarthStore/Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmarthStore.Models;
+
+namespace SmarthStore.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
