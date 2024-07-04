bd103b9ad7ead94b867c1d7abd2baa9ab81600d8
Lucas Woibau <lucaswoibau7@gmail.com>
2024-07-03 21:40:14 -0300

    List of products view

---
+++ SmarthStore/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmarthStore.Services;
+
+namespace SmarthStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductsController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.OrderByDescending(p => p.Id).ToList();
+            return View(products);
+        }
+    }
+}
