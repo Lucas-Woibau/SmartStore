b9ea7484fadaf109f4c6605e6fb59f3725a8fea2
Lucas Woibau <lucaswoibau7@gmail.com>
2024-08-17 15:32:30 -0300

    Add Search Sort and Filter Functionalities and Details

--- SmartStore/Controllers/StoreController.cs
+++ SmartStore/Controllers/StoreController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SmarthStore.Models;
 using SmarthStore.Services;
+using SmartStore.Models;
 
 namespace SmartStore.Controllers
 {
@@ -8,32 +9,78 @@ namespace SmartStore.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly int pageSize = 8;
-        public StoreController(ApplicationDbContext context) 
-        { 
+        public StoreController(ApplicationDbContext context)
+        {
             _context = context;
         }
-        public IActionResult Index(int pageIndex)
+        public IActionResult Index(int pageIndex, string? search, string? brand, string? category, string? sort)
         {
             IQueryable<Product> query = _context.Products;
 
-            query = query.OrderByDescending(p => p.Id);
+            if (search != null && search.Length > 0)
+            {
+                query = query.Where(p => p.Name.Contains(search));
+            }
 
-            if(pageIndex < 1) 
+            if (brand != null && brand.Length > 0)
+            {
+                query = query.Where(p => p.Brand.Contains(brand));
+            }
+
+            if (category != null && category.Length > 0)
+            {
+                query = query.Where(p => p.Category.Contains(category));
+            }
+
+            // sort
+            if (sort == "price_asc")
+            {
+                query = query.OrderBy(p => p.Price);
+            }
+            else if (sort == "price_desc")
+            {
+                query = query.OrderByDescending(p => p.Price);
+            }
+            else
+            {
+                query = query.OrderByDescending(p => p.Id);
+            }
+
+            if (pageIndex < 1)
             {
                 pageIndex = 1;
             }
 
             decimal count = query.Count();
             int totalPages = (int)Math.Ceiling(count / pageSize);
-            query = query.Skip((pageIndex - 1) * pageIndex).Take(pageSize);   
+            query = query.Skip((pageIndex - 1) * pageIndex).Take(pageSize);
 
             var products = query.ToList();
 
             ViewBag.Products = products;
-            ViewBag.PageIndex= pageIndex;
+            ViewBag.PageIndex = pageIndex;
             ViewBag.TotalPages = totalPages;
 
-            return View();
+            var storeSearchModel = new StoreSearchModel()
+            {
+                Search = search,
+                Brand = brand,
+                Category = category,
+                Sort = sort
+            };
+
+            return View(storeSearchModel);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null) 
+            {
+                return RedirectToAction("Index", "Store");
+            }
+
+            return View(product);
         }
     }
 }
