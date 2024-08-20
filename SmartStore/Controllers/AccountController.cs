e5c321d25c6888f2d39fd3f055f793d24cba3cbc
Lucas Woibau <lucaswoibau7@gmail.com>
2024-08-20 14:14:10 -0300

    User Registration - Create the Register Form

---
+++ SmartStore/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SmartStore.Models;
+
+namespace SmartStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
