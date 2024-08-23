0e46f52e21b49bafa85e06c14e17a7c44e0d8350
Lucas Woibau <93388159+Lucas-Woibau@users.noreply.github.com>
2024-08-23 16:04:03 -0300

    Display Registered Users

---
+++ SmartStore/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SmartStore.Models;
+
+namespace SmartStore.Controllers
+{
+    [Authorize(Roles = "admin")]
+    [Route("/Admin/[controller]/{action=Index}/{id?}")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public UserController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.OrderByDescending(u => u.CreatedAt).ToList();
+            return View(users);
+        }
+    }
+}
