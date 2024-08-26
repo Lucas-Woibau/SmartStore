db39f8cde29d79616010661cfc3f1b9c15e03c17
Lucas Woibau <93388159+Lucas-Woibau@users.noreply.github.com>
2024-08-26 14:46:36 -0300

    Update User Role and Delete User Account

--- SmartStore/Controllers/UserController.cs
+++ SmartStore/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SmartStore.Models;
 using System.Linq;
 
@@ -41,22 +42,101 @@ namespace SmartStore.Controllers
 
         public async Task<IActionResult> Details(string? id)
         {
-            if(id == null)
+            if (id == null)
             {
-                return RedirectToAction("Index", "Users");
+                return RedirectToAction("Index", "User");
             }
 
             var appUser = await _userManager.FindByIdAsync(id);
 
             if (appUser == null)
             {
-                return RedirectToAction("Index", "Users");
+                return RedirectToAction("Index", "User");
             }
 
             ViewBag.Roles = await _userManager.GetRolesAsync(appUser);
 
+            // Get available roles
+            var availableRoles = _roleManager.Roles.ToList();
+            var items = new List<SelectListItem>();
+            foreach (var role in availableRoles)
+            {
+                items.Add(
+                    new SelectListItem
+                    {
+                        Text = role.NormalizedName,
+                        Value = role.Name,
+                        Selected = await _userManager.IsInRoleAsync(appUser, role.Name!),
+                    });
+            }
+
+            ViewBag.SelectItems = items;
+
             return View(appUser);
         }
 
+        public async Task<IActionResult> EditRole(string? id, string? newRole)
+        {
+            if (id == null || newRole == null)
+            {
+                return RedirectToAction("Index", "User");
+            }
+
+            var roleExists = await _roleManager.RoleExistsAsync(newRole);
+            var appUser = await _userManager.FindByIdAsync(id);
+
+            if (appUser == null || !roleExists)
+            {
+                return RedirectToAction("Index", "User");
+            }
+
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser!.Id == appUser.Id)
+            {
+                TempData["ErrorMessage"] = "You cannot update your own role!";
+                return RedirectToAction("Details", "User", new { id });
+            }
+
+            // Update user role
+            var userRoles = await _userManager.GetRolesAsync(appUser);
+            await _userManager.RemoveFromRolesAsync(appUser, userRoles);
+            await _userManager.AddToRoleAsync(appUser, newRole);
+
+            TempData["SuccessMessage"] = "User Role updated successfully";
+            return RedirectToAction("Details", "User", new { id });
+        }
+
+        public async Task<IActionResult> DeleteAccount(string? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index", "User");
+            }
+
+            var appUser = await _userManager.FindByIdAsync(id);
+
+            if (appUser == null)
+            {
+                return RedirectToAction("Index", "User");
+            }
+
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser!.Id == appUser.Id)
+            {
+                TempData["ErrorMessage"] = "You cannot delete your own role!";
+                return RedirectToAction("Details", "User", new { id });
+            }
+
+            // Delete
+            var result = await _userManager.DeleteAsync(appUser);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "User");
+            }
+
+            TempData["ErrorMessage"] = "Unable to delete this account: " + result.Errors.First().Description;
+
+            return RedirectToAction("Details", "User", new { id });
+        }
     }
 }
