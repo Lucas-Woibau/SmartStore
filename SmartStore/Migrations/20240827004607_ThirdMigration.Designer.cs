5438f44fa436b82ff825b54d041b5e0094ac8bad
Lucas Woibau <93388159+Lucas-Woibau@users.noreply.github.com>
2024-08-26 21:47:16 -0300

    Create Entity Models and Tables

--- SmartStore/Migrations/20240827004607_ThirdMigration.Designer.cs
+++ SmartStore/Migrations/20240827004607_ThirdMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SmartStore.Services;
 
@@ -11,9 +12,11 @@ using SmartStore.Services;
 namespace SmartStore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240827004607_ThirdMigration")]
+    partial class ThirdMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SmartStore/Services/ApplicationDbContext.cs
+++ SmartStore/Services/ApplicationDbContext.cs
@@ -12,5 +12,6 @@ namespace SmartStore.Services
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }
