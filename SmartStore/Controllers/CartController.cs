ad82c9d1a3baac1ef77737ae2dbd7bed49a6141d
Lucas Woibau <93388159+Lucas-Woibau@users.noreply.github.com>
2024-08-28 19:32:32 -0300

    Shopping Cart - Display Items

---
+++ SmartStore/Controllers/CartController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SmartStore.Models;
+using SmartStore.Services;
+
+namespace SmartStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly decimal _shippingFee;
+
+        public CartController(ApplicationDbContext context, IConfiguration configuration, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _shippingFee = configuration.GetValue<decimal>("CartSettings:ShippingFee");
+
+        }
+        public IActionResult Index()
+        {
+            List<OrderItem> cartItems = CartHelper.GetCartItems(Request, Response, _context);
+            decimal subtotal = CartHelper.GetSubtotal(cartItems);
+
+            ViewBag.CartItems = cartItems;
+            ViewBag.Subtotal = subtotal;
+            ViewBag.ShippingFee = _shippingFee;
+            ViewBag.Total = subtotal + _shippingFee;
+
+            return View();
+        }
+    }
+}
--- SmartStore/Services/CartHelper.cs
+++ SmartStore/Services/CartHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using SmartStore.Models;
+using System.Text.Json;
 
 namespace SmartStore.Services
 {
@@ -42,6 +43,44 @@ namespace SmartStore.Services
             }
 
             return cartSize;
+        }
+
+        public static List<OrderItem> GetCartItems(HttpRequest request, HttpResponse response, ApplicationDbContext context)
+        {
+            var cartItems = new List<OrderItem>();
+
+            var cartDictionary = GetCartDictionary(request, response);
+
+            foreach (var pair in cartDictionary)
+            {
+                int productId = pair.Key;
+                int quantity = pair.Value;
+                var product = context.Products.Find(productId);
+                if (product == null) continue;
+
+                var item = new OrderItem
+                {
+                    Quantity = quantity,
+                    UnitPrice = product.Price,
+                    Product = product,
+                };
+
+                cartItems.Add(item);
+            }
+
+            return cartItems;
+        }
+
+        public static decimal GetSubtotal(List<OrderItem> cartItems)
+        {
+            decimal subtotal = 0;
+
+            foreach ( var item in cartItems )
+            {
+                subtotal += item.Quantity * item.UnitPrice;
+            }
+
+            return subtotal;
         }
     }
 }
