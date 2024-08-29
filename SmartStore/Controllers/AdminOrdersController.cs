0e3e2482df6ace2aa4e08178a95d2791e2b27af2
Lucas Woibau <93388159+Lucas-Woibau@users.noreply.github.com>
2024-08-29 18:27:59 -0300

    Admin Orders - Display Orders to Admin

---
+++ SmartStore/Controllers/AdminOrdersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartStore.Models;
+using SmartStore.Services;
+
+namespace SmartStore.Controllers
+{
+    [Authorize(Roles = "admin")]
+    [Route("/Admin/Orders/{action=Index}/{id?}")]
+    public class AdminOrdersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly int _pageSize = 5;
+
+        public AdminOrdersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(int pageIndex)
+        {
+            IQueryable<Order> query = _context.Orders.Include(o => o.Client)
+                .Include(o => o.Items).OrderByDescending(o => o.Id);
+
+            if (pageIndex <= 0)
+            {
+                pageIndex = 1;
+            }
+            decimal count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / _pageSize);
+
+            query = query.Skip((pageIndex - 1) * _pageSize).Take(_pageSize);
+
+            var orders = query.ToList();
+
+            ViewBag.Orders = orders;
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.TotalPages = totalPages;
+
+            return View();
+        }
+    }
+}
